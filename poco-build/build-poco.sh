2aa8acfb11fb0b1e430b2201281972578d14da19
lxy <li-cannon@163.com>
2019-04-26 17:53:17 +0800

    fix

--- poco-build/build-poco.sh
+++ poco-build/build-poco.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export PATH=$PATH:/home/standalone-toolchain/bin
+export PATH=$PATH:/root/poco-build/standalone-toolchain/bin
 cd poco-poco-1.9.0-release/
 #./configure CFLAGS="-std=c99" --config=Android --no-samples --no-tests  --shared --include-path=/usr/local/ssl/include --library-path=/usr/local/ssl/lib 
 ./configure --config=Android --no-samples --no-tests --no-ipv6 --no-sharedmemory --omit=Data/MySQL,Data/ODBC --shared --include-path=/usr/local/ssl/include --library-path=/usr/local/ssl/lib --cflags=-std=c99
