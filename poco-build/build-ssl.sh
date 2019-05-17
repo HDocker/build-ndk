7389e2a7bbfd6b53b874eb34486521158a884865
lxy <li-cannon@163.com>
2019-05-17 11:51:38 +0800

    fix android ssl build soname

--- poco-build/build-ssl.sh
+++ poco-build/build-ssl.sh
@@ -5,5 +5,5 @@ perl -pi -e 's/install: all install_docs install_sw/install: install_docs instal
 ./config shared no-ssl2 no-ssl3 no-comp no-hw no-engine --openssldir=/usr/local/ssl --prefix=/usr/local/ssl
 
 make depend
-make all
+make CALC_VERSIONS="SHLIB_COMPAT=; SHLIB_SOVER=" all
 make install CC=$ANDROID_TOOLCHAIN/arm-linux-androideabi-gcc RANLIB=$ANDROID_TOOLCHAIN/arm-linux-androideabi-ranlib
